0673b034ac7e3e320dc24ba56588797da72e3ce9
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2023-12-16 12:25:40 -0400

    [Feature] Cost Function - Univariate Linear Regression
    Finished implementing cost function calculation for univariate linear regression.

---
+++ supervised-learning/cost-function.py
@@ -0,0 +1,32 @@
+# Goals: Implement and explore the cost function for linear regression with one variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480, 430, 630, 730])
+
+
+def compute_cost(x, y, w, b):
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+        x (ndarray (m,)): Data, m examples
+        y (ndarray (m,)): target values
+        w,b (scalar)    : model parameters
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for
+        linear regression to fit the data points in x and y
+    """
+    # number of training examples
+    m = len(x)
+
+    cost_sum = 0
+    for i in range(x):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum += cost
+    total_cost = (1 / (2 * m)) * cost_sum
+
+    return total_cost

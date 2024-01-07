c2b01aad909d85588736be927a0d240119b6db4a
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-01-07 15:44:29 -0400

    [Feature] Vectorization vs. Manual Iteration – Dot Product
    Time comparison between manual iteration for computing dot product versus utilizing vectorization to utilize GPU for dot product.

--- supervised-learning/numpy_examples.py
+++ supervised-learning/numpy_examples.py
@@ -85,3 +85,55 @@ except Exception as e:
 a = np.array([1, 2, 3, 4])
 b = 5 * a
 print(f"b = 5 * a : {b}")
+
+
+# Dot Product Comparison
+# Manual dot product that does not use vectorization
+def my_dot(a, b):
+    """
+    Compute the dot product of two vectors
+
+    Args:
+        a (ndarray(n,)):    input vector
+        b (ndarray(n,)):    input vector
+
+    Returns:
+        x (scalar):
+    """
+    x = 0
+    for i in range(a.shape[0]):
+        x = x + a[i] * b[i]
+    return x
+
+
+a = np.array([1, 2, 3, 4])
+b = np.array([-1, 4, 3, 2])
+print(f"my_dot(a, b) = {my_dot(a, b)}")
+
+# Using np.dot
+c = np.dot(a, b)
+print(f"NumPy 1-D np.dot(a, b) = {c}, np.dot(a, b).shape = {c.shape} ")
+c = np.dot(b, a)
+print(f"NumPy 1-D np.dot(b, a) = {c}, np.dot(a, b).shape = {c.shape} ")
+
+# Time comparison: Manual vs. vectorization dot product
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+c = my_dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"my_dot(a, b) =  {c:.4f}")
+print(f"loop version duration: {1000*(toc-tic):.4f} ms ")
+
+del a
+del b  # remove these big arrays from memory

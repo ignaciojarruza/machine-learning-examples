f67e3076fa3bcf1e7af491931a850be07a828cda
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-01-07 15:35:51 -0400

    [Functions] Single Vector Operations
    Review of Simple vector operations using Numpy. sum, mean, exponent, negation

--- supervised-learning/numpy_examples.py
+++ supervised-learning/numpy_examples.py
@@ -57,3 +57,15 @@ print("a[:3]    = ", c)
 # access all elements
 c = a[:]
 print("a[:]     = ", c)
+
+# Single Vector Operations
+a = np.array([1, 2, 3, 4])
+print(f"a             : {a}")
+b = -a
+print(f"b = -a        : {b}")
+b = np.sum(a)
+print(f"b = np.sum(a) : {b}")
+b = np.mean(a)
+print(f"b = np.mean(a): {b}")
+b = a**2
+print(f"b = a**2      : {b}")
